c4d3d2fba3782c834b0b1cca1deaf198b8ba507f
Gökdeniz SARİİBRAHİM <gkdnzsrbrhm@gmail.com>
2022-07-25 17:44:52 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basic_app/image_widget.dart';
+import 'package:flutter_basic_app/my_home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,63 +12,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("İmage Examples"),
+        ),
+        body: ImageExample(),
+      ),
       title: "My Counter App",
       theme: ThemeData(
           primarySwatch: Colors.purple,
           textTheme: TextTheme(
               headline1: TextStyle(
                   color: Colors.orange.shade400, fontWeight: FontWeight.bold))),
-      home: MyHomePage(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _sayac = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        child: Scaffold(
-      appBar: AppBar(
-        title: Text("My Counter AppBar"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Number of button presses",
-              style: TextStyle(
-                fontSize: 24,
-                color: Colors.red.shade600,
-              ),
-            ),
-            Text(
-              _sayac.toString(),
-              style: Theme.of(context).textTheme.headline1,
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          sayaciarttir();
-        },
-        child: Icon(Icons.add),
-      ),
-    ));
-  }
-
-  void sayaciarttir() {
-    setState(() {});
-    _sayac--;
   }
 }
